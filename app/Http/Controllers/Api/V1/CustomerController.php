d4b1bbb672768a9d4139712d11a25e2581c7f8ac
Naseeb Khan <110621136+Naseebullah-Khan@users.noreply.github.com>
2026-02-23 00:16:36 +0430

    Manipulating Data - Creating Resources With POST Requests

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Requests\V1\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
@@ -33,19 +33,11 @@ class CustomerController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreCustomerRequest $request)
+    public function store(StoreCustomerRequest $request): CustomerResource
     {
-        //
+        return new CustomerResource(resource: Customer::create(attributes: $request->all()));
     }
 
     /**
@@ -58,14 +50,6 @@ class CustomerController extends Controller
             return new CustomerResource(resource: $customer->loadMissing(relations: "invoices"));
         }
         return new CustomerResource(resource: $customer);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Customer $customer)
-    {
-        //
     }
 
     /**
