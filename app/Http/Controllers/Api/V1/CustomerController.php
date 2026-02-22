3489c3f4a530037e7746a980ba01483b99b04647
Naseeb Khan <110621136+Naseebullah-Khan@users.noreply.github.com>
2026-02-23 00:35:19 +0430

    Manipulating Data - Updating With PUT and PATCH

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\StoreCustomerRequest;
-use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
 use App\Models\Customer;
@@ -55,9 +55,9 @@ class CustomerController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateCustomerRequest $request, Customer $customer)
+    public function update(UpdateCustomerRequest $request, Customer $customer): void
     {
-        //
+        $customer->update($request->all());
     }
 
     /**
